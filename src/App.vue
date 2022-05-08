0dbc987e05c1be31a260f8c171348ae50ff81cbd
Sanjay Sudhakaran <sanjaysudhakaran@gmail.com>
2022-05-08 12:23:14 +1200

    transactions showing checkpoint

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <FileParser @file-parsed-event="initialiseTxsData" />
+  <section>
+    <div>
+      <p><br /></p>
+    </div>
+  </section>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <TxFilter :filterFunc="this.filter" />
+        </div>
+        <div class="col">
+          <TransactionRows :txs="this.txs" />
+          <!-- This should take in an already filtered array not the original txs -->
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FileParser from "./components/FileParser";
+import TransactionRows from "./components/TransactionRows";
+import TxFilter from "./components/TxFilter.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FileParser,
+    TransactionRows,
+    TxFilter,
+  },
+  methods: {
+    filter(searchTerm) {
+      this.filteredTxs = this.filteredTxs.filter(
+        (tx) => tx.name.indexOf(searchTerm) > -1
+      );
+    },
+    initialiseTxsData(arr) {
+      arr.forEach((element) => {
+        console.log(element);
+      });
+      this.txs = arr;
+    },
+  },
+  data() {
+    return {
+      txs: [],
+      uncategorisedTxs: [], // Remaining txs after some are categorised
+      filteredTxs: [], // temporary
+    };
+  },
+};
 </script>
 
 <style>
