1bacb96a1dad904d09c93d9d31c3237d0c3a90e4
Sanjay Sudhakaran <sanjaysudhakaran@gmail.com>
2022-05-16 17:06:59 +1200

    plotly working checkpoint

--- src/App.vue
+++ src/App.vue
@@ -60,6 +60,17 @@
       </div>
     </div>
   </section>
+  <section>
+    <button
+      @click="getInsights()"
+      type="button"
+      class="btn btn-primary mt-2 mb-2"
+    >
+      Show insights!
+    </button>
+    <h2>Insights</h2>
+    <div id="graph"></div>
+  </section>
 </template>
 
 
@@ -68,6 +79,7 @@ import FileParser from "./components/FileParser";
 import TransactionRows from "./components/TransactionRows";
 import TxFilter from "./components/TxFilter.vue";
 import CategoryView from "./components/CategoryView.vue";
+import Plotly from "plotly.js-dist/plotly";
 
 export default {
   name: "App",
@@ -97,30 +109,9 @@ export default {
       this.categorised.push(categorisedQuery);
     },
     updateCategorisedTxs(txObject) {
-      // const txArray = txObject.txs;
-      // const txCateogry = txObject.
-
       const { category, txs } = txObject;
 
       this.categorisedTxs[category].push(...txs);
-
-      // switch (txObject.category) {
-      //   case "housing":
-      //     this.categorisedTxs.housing.push(...txArray); // extends an array with spread operator
-      // }
-
-      // const filterArray = (arr1, categoriesObj) => {
-      //   const filtered = arr1.filter((el) => {
-      //     for (const category in categoriesObj) {
-      //       console.log(category);
-      //       if (categoriesObj[category].includes(el)) {
-      //         console.log("FOUND");
-      //       }
-      //       return !categoriesObj[category].includes(el);
-      //     }
-      //   });
-      //   return filtered;
-      // };
 
       const filterArray = (arr1, categoriesObj) => {
         let filtered = [...arr1];
@@ -142,6 +133,65 @@ export default {
       );
       this.uncategorisedTxs = filteredArray;
       this.filteredTxs = this.uncategorisedTxs;
+    },
+    getInsights() {
+      // console.log(JSON.stringify(this.categorisedTxs));
+      let monthlyData = {};
+      for (const category in this.categorisedTxs) {
+        console.log(JSON.stringify(this.categorisedTxs[category]));
+        this.categorisedTxs[category].forEach((el) => {
+          let date = el.Date.replace("/", "-").replace("/", "");
+          date = date.slice(0, 7);
+          if (!(date in monthlyData)) monthlyData[date] = 0;
+          let amount = el["Amount\r"].replace("-", "").replace("\r", "");
+          amount = parseFloat(amount);
+          monthlyData[date] += amount;
+        });
+      }
+
+      console.log("Monthly data", JSON.stringify(monthlyData));
+
+      var trace1 = {
+        x: Object.keys(monthlyData),
+        y: Object.values(monthlyData),
+        name: "Expenses",
+        // marker: { color: "rgb(55, 83, 109)" },
+        marker: { color: "rgb(26, 118, 255)" },
+        type: "bar",
+      };
+
+      var data = [trace1];
+
+      var layout = {
+        title: "Total Expenses by Month",
+        xaxis: {
+          tickfont: {
+            size: 14,
+            color: "rgb(107, 107, 107)",
+          },
+          type: "category",
+        },
+        yaxis: {
+          title: "NZD ($)",
+          titlefont: {
+            size: 16,
+            color: "rgb(107, 107, 107)",
+          },
+          tickfont: {
+            size: 14,
+            color: "rgb(107, 107, 107)",
+          },
+        },
+        legend: {
+          x: 0,
+          y: 1.0,
+          bgcolor: "rgba(255, 255, 255, 0)",
+          bordercolor: "rgba(255, 255, 255, 0)",
+        },
+        bargap: 0.15,
+      };
+
+      Plotly.newPlot("graph", data, layout);
     },
   },
   data() {
---
+++ src/components/CategoryView.vue
@@ -0,0 +1,51 @@
+<template>
+  <h4>{{ categoryName }}</h4>
+  <p>Total: {{ total }}</p>
+  <button @click="addTx(tx)" type="button" class="btn btn-primary mt-2 mb-2">
+    {{ categoryName }}
+  </button>
+  <p>{{ this.queries }}</p>
+</template>
+
+<script>
+export default {
+  name: "CategoryView",
+  emits: ["query-added-event", "tx-categorised-event"],
+  props: {
+    categoryName: String,
+    propertyName: String,
+    tx: Array,
+    query: String,
+  },
+  methods: {
+    addTx(txArray) {
+      this.queries.push(this.query);
+      this.$emit("query-added-event", this.query);
+
+      txArray.forEach((tx) => {
+        const amount = tx["Amount\r"].replace("-", "");
+        this.txs.push(Number(amount));
+
+        this.categorisedTxs.category = this.propertyName;
+        this.categorisedTxs.txs.push(tx); // so we can filter these out in App.js
+      });
+
+      this.$emit("tx-categorised-event", this.categorisedTxs);
+    },
+  },
+  computed: {
+    total() {
+      return this.txs.reduce(
+        (previousValue, currentValue) => previousValue + currentValue
+      );
+    },
+  },
+  data() {
+    return {
+      txs: [0],
+      queries: [],
+      categorisedTxs: { category: this.propertyName, txs: [] },
+    };
+  },
+};
+</script>
