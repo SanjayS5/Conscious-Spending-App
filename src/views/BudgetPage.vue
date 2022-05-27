c7935b8e7a6f7af5867b4198a0115e0914f585cd
Sanjay Sudhakaran <sanjaysudhakaran@gmail.com>
2022-05-27 13:59:34 +1200

    adding budget page to git

---
+++ src/views/BudgetPage.vue
@@ -0,0 +1,107 @@
+<template>
+  <section class="container">
+    <section>
+      <div class="mb-3">
+        <router-link
+          :to="{
+            name: 'Home',
+          }"
+          >Home Page
+        </router-link>
+        >
+        <router-link
+          :to="{
+            name: 'categorisePage',
+          }"
+          >Categorise Transactions
+        </router-link>
+        >
+        <router-link
+          :to="{
+            name: 'Insights',
+          }"
+        >
+          Insights
+        </router-link>
+        > Budget
+      </div>
+    </section>
+    <section>
+      <div class="mb-3"><h1>Budget</h1></div>
+      <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link active"
+            id="home-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#home"
+            type="button"
+            role="tab"
+            aria-controls="home"
+            aria-selected="true"
+          >
+            Home
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link"
+            id="profile-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#profile"
+            type="button"
+            role="tab"
+            aria-controls="profile"
+            aria-selected="false"
+          >
+            Profile
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link"
+            id="contact-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#contact"
+            type="button"
+            role="tab"
+            aria-controls="contact"
+            aria-selected="false"
+          >
+            Contact
+          </button>
+        </li>
+      </ul>
+      <div class="tab-content" id="myTabContent">
+        <div
+          class="tab-pane fade show active"
+          id="home"
+          role="tabpanel"
+          aria-labelledby="home-tab"
+        >
+          ...
+        </div>
+        <div
+          class="tab-pane fade"
+          id="profile"
+          role="tabpanel"
+          aria-labelledby="profile-tab"
+        >
+          ...
+        </div>
+        <div
+          class="tab-pane fade"
+          id="contact"
+          role="tabpanel"
+          aria-labelledby="contact-tab"
+        >
+          ...
+        </div>
+      </div>
+    </section>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
