50263d4ff374941b6c5399e098f935dbc988cd1a
Sanjay Sudhakaran <sanjaysudhakaran@gmail.com>
2022-05-27 13:52:06 +1200

    breadcrumbs to budget page

--- src/router/index.js
+++ src/router/index.js
@@ -17,17 +17,6 @@ const routes = [
         name: 'categorisePage',
         component: CategorisePage,
     },
-    // {
-    //     // path: '/categorise/:uncategorisedTxs/:txs/:filteredTxs',
-    //     path: '/categorise/:parsedTxs',
-    //     name: 'categorisePage',
-    //     component: CategorisePage,
-    //     props: route => ({
-    //         // uncategorisedTxs: JSON.parse(route.params.uncategorisedTxs),
-    //         // filteredTxs: JSON.parse(route.params.filteredTxs),
-    //         parsedTxs: JSON.parse(route.params.parsedTxs)
-    //     })
-    // },
     {
         path: '/InsightsPage/:categorisedTxs',
         name: 'Insights',
